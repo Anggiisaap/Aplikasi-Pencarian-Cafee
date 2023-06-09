36d6e2ce0af16e842e8c7b6fccc129153cd66349
Anggiisaap <129248514+Anggiisaap@users.noreply.github.com>
2023-06-09 16:10:42 +0700

    Add files via upload

---
+++ lib/widgets/NavBar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Anggi Saputra'),
+            accountEmail: Text('anggisptra1616@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  "https://picsum.photos/200/300",
+                  fit: BoxFit.cover,
+                  width: 90,
+                  height: 90,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green[600],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.favorite),
+            title: const Text('Favorites'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Share'),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Request'),
+            onTap: () => null,
+            trailing: ClipOval(
+              child: Container(
+                color: Colors.red,
+                width: 20,
+                height: 20,
+                child: const Center(
+                  child: Text(
+                    '8',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => null,
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Exit'),
+            leading: Icon(Icons.exit_to_app),
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
+}
